8cfc90dbd05d693995b30808c9ac073b41724345
abhishekpr-devops <273493196+abhishekpr-devops@users.noreply.github.com>
2026-05-23 16:38:46 +0530

    Revert to 2D and add fullscreen layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import React, { useState } from "react";
 import PhaserGame from "./game/PhaserGame";
-import Game3D from "./game3d/Game3D";
-import './styles/App.css';
 
-export default function App() {
-  const [show3D, setShow3D] = useState(false);
-
-  return (
-    <div className="app">
-      <button
-        className="toggle-btn"
-        onClick={() => setShow3D((prev) => !prev)}
-        style={{ position: "absolute", top: 10, left: 10, zIndex: 1000 }}
-      >
-        {show3D ? "Switch to 2D" : "Switch to 3D"}
-      </button>
-      {show3D ? <Game3D /> : <PhaserGame />}
-    </div>
-  );
+function App() {
+  return <PhaserGame />;
 }
+
+export default App;
